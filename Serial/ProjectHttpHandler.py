f198ced50f4ecc89d6310044498262fc378a2245
sj2565 <seojoon2565@gmail.com>
2025-01-25 04:31:41 +0900

    Python Serial 통신

---
+++ Serial/ProjectHttpHandler.py
@@ -0,0 +1,15 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class ProjectHttpHandler(SimpleHTTPRequestHandler):
+      def do_GET(self):            
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            output = '<html><head><meta http-equiv="Content="text/html" charset="UTF-8">'
+            output += '<title>My Web Server</title></head>'
+            output += '<body>'
+            output += '<p>Request: %s</p>' % (self.path)
+            output += '<p>저장된 데이터 개수 : %d </p>' % (self.server.gateway.countDb())
+            output += self.server.gateway.makeHtmlTableDb()
+            output += '</body></html>'
+            self.wfile.write(output.encode())
